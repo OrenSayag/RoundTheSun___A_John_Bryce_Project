ca22a7e67ac3a60f63cdcdd339d1f5f948d074ae
Oren Sayag <orensayag500@gmail.com>
2021-07-14 12:03:00 +0300

    140721_1202

--- DB/config.js
+++ DB/config.js
@@ -4,11 +4,30 @@ const mysql = require("mysql");
 const env = require('../env')
 
 
-let pool = mysql.createPool({
-  host: 'us-cdbr-east-04.cleardb.com',
-  user: 'bc8202b70b5cc9',
-  password: '4bb08b17',
-  database: "heroku_60bc19962800d65",
+// let pool = mysql.createPool({
+// //   host: 'us-cdbr-east-04.cleardb.com',
+// //   user: 'bc8202b70b5cc9',
+// //   password: '4bb08b17',
+// //   database: "heroku_60bc19962800d65",
+//   host: '159.65.92.190',
+//   user: 'windos',
+//   password: '1234',
+//   database: "orensayag_project3_initial",
+// //   host: 'localhost',
+// //   user: 'root',
+// //   password: '',
+// //   database: "orensayag_project3_initial",
+// });
+
+let con = mysql.createConnection({
+//   host: 'us-cdbr-east-04.cleardb.com',
+//   user: 'bc8202b70b5cc9',
+//   password: '4bb08b17',
+//   database: "heroku_60bc19962800d65",
+  host: '159.65.92.190',
+  user: 'windows',
+  password: '1234',
+  database: "orensayag_project3_initial",
 //   host: 'localhost',
 //   user: 'root',
 //   password: '',
@@ -19,22 +38,22 @@ let pool = mysql.createPool({
 // user = bc8202b70b5cc9
 // password = 4bb08b17
 
-// con.connect(function (err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
+con.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err);
 
-//     con = reconnect(con);
+    // con = reconnect(con);
     
-//     return;
-//   }
+    return;
+  }
 
-//   console.log("Connected to mysql");
-// });
+  console.log("Connected to mysql");
+});
 
 // wrapper
 const myQuery = (q) => {
     return new Promise((resolve, reject)=>{
-        pool.query(q,(err,results)=>{
+        con.query(q,(err,results)=>{
             if(err){
                 reject(err)
             }else{
@@ -44,27 +63,27 @@ const myQuery = (q) => {
     })
 }
 
-function reconnect(connection){
-  console.log("\n New connection tentative...");
+// function reconnect(connection){
+//   console.log("\n New connection tentative...");
 
-  //- Destroy the current connection variable
-  if(connection) connection.destroy();
+//   //- Destroy the current connection variable
+//   if(connection) connection.destroy();
 
-  //- Create a new one
-  var connection = mysql_npm.createConnection(db_config);
+//   //- Create a new one
+//   var connection = mysql_npm.createConnection(db_config);
 
-  //- Try to reconnect
-  connection.connect(function(err){
+//   //- Try to reconnect
+//   connection.connect(function(err){
     
-      if(err) {
-          //- Try to connect every 2 seconds.
-          setTimeout(reconnect, 2000);
-      }else {
-          console.log("\n\t *** New connection established with the database. ***")
-          return connection;
-      }
-  });
-}
+//       if(err) {
+//           //- Try to connect every 2 seconds.
+//           setTimeout(reconnect, 2000);
+//       }else {
+//           console.log("\n\t *** New connection established with the database. ***")
+//           return connection;
+//       }
+//   });
+// }
 
 
 // EXPORT
--- routes/auth.js
+++ routes/auth.js
@@ -83,6 +83,7 @@ router.post("/signup", async (req, res) => {
     }
     return res.sendStatus(200);
   } catch (error) {
+    console.log(error)
     return res.status(500).send({ error });
   }
 });
